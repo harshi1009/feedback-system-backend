d7888c55f9695dbaa5f05e5bb5756148f0d0d161
Harshitha32480 <153381078+harshi1009@users.noreply.github.com>
2024-12-10 11:13:19 +0530

    Update CriteriaController.java

--- src/main/java/com/feedback/controller/CriteriaController.java
+++ src/main/java/com/feedback/controller/CriteriaController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/criteria")
-@CrossOrigin(origins = "https://vamshikumar32501.github.io/student-feedback-frontend") // Update with your frontend URL
+@CrossOrigin(origins = "https://vamshikumar32501.github.io") // Update with your frontend URL
 public class CriteriaController {
 
     @Autowired
