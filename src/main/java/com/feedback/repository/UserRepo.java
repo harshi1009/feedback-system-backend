0cce8bd7d617d526d55e5c062020f10b28467d8a
Harshitha32480 <153381078+harshi1009@users.noreply.github.com>
2024-12-11 13:51:08 +0530

    Update UserRepo.java

--- src/main/java/com/feedback/repository/UserRepo.java
+++ src/main/java/com/feedback/repository/UserRepo.java
@@ -11,5 +11,7 @@ public interface UserRepo extends JpaRepository<User, Long> {
     // Method to find a user by email, returning an Optional<User>
     Optional<User> findByEmail(String email);
     Optional<User> findByUsername(String username);
+    boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
     long countByUserType(String userType);
 }
